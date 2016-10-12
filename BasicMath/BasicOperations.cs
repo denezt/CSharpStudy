3c77e89ae09e357d20716239aac00fabd69e185e
Richard L. Jackson <denezt@yahoo.com>
2016-10-12 08:18:30 +0200

    Changing indentation

--- BasicMath/BasicOperations.cs
+++ BasicMath/BasicOperations.cs
@@ -1,7 +1,6 @@
 /*
 * Basic Math Operation and
 * Possible logical implementations.
-*
 */
 using System;
 
@@ -11,27 +10,26 @@ namespace SimpleOperations
     {
         static void Main(string[] args)
         {
-		Console.WriteLine("5 + 3 = " + ( 5 + 3));
-    Console.WriteLine("5 - 3 = " + ( 5 - 3));
-    Console.WriteLine("5 * 3 = " + ( 5 * 3));
-    Console.WriteLine("5 / 3 = " + ( 5 / 3));
-    Console.WriteLine("5.2 % 3 = " + ( 5.2 % 3));
+      		Console.WriteLine("5 + 3 = " + ( 5 + 3));
+          Console.WriteLine("5 - 3 = " + ( 5 - 3));
+          Console.WriteLine("5 * 3 = " + ( 5 * 3));
+          Console.WriteLine("5 / 3 = " + ( 5 / 3));
+          Console.WriteLine("5.2 % 3 = " + ( 5.2 % 3));
 
-    int i = 0;
-    Console.WriteLine("i++ = " + (i++));
-    Console.WriteLine("++i = " + (++i));
-    Console.WriteLine("i-- = " + (i--));
-    Console.WriteLine("--i = " + (--i));
+          int i = 0;
+          Console.WriteLine("i++ = " + (i++));
+          Console.WriteLine("++i = " + (++i));
+          Console.WriteLine("i-- = " + (i--));
+          Console.WriteLine("--i = " + (--i));
 
-    Console.WriteLine("(i += 3) -> equals: " + (i += 3));
-    Console.WriteLine("(i -= 2) -> equals: " + (i -= 2));
-    Console.WriteLine("(i *= 2) -> equals: " + (i *= 2));
-    Console.WriteLine("(i /= 2) -> equals: " + (i /= 2));
-    Console.WriteLine("(i /= 2) -> equals: " + (i %= 2));
+          Console.WriteLine("(i += 3) -> equals: " + (i += 3));
+          Console.WriteLine("(i -= 2) -> equals: " + (i -= 2));
+          Console.WriteLine("(i *= 2) -> equals: " + (i *= 2));
+          Console.WriteLine("(i /= 2) -> equals: " + (i /= 2));
+          Console.WriteLine("(i /= 2) -> equals: " + (i %= 2));
 
-    Console.WriteLine("Done!\nPress any key to continue!");
-
-		Console.ReadKey();
+          Console.WriteLine("Done!\nPress any key to continue!");
+      		Console.ReadKey();
         }
     }
 }
--- CastingTypes/Castings.cs
+++ CastingTypes/Castings.cs
@@ -1,4 +1,5 @@
-// A Hello World! program in C#.
+// Useful for casting (Changing/Converting datatypes).
+
 using System;
 namespace Castings
 {
